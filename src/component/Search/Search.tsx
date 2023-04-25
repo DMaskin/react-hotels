46c5336a8555ce4dd7281192a16ce63032bec0dc
Dmitriy Maskin <36866382+DMaskin@users.noreply.github.com>
2023-04-26 00:38:15 +0300

    Merge pull request #3
    * add min date & max date + RU locale for DatePicker
    
    * add search validation and fix layout

--- src/component/Search/Search.tsx
+++ src/component/Search/Search.tsx
@@ -1,5 +1,6 @@
 import React, { FormEvent, ForwardedRef, forwardRef, useState } from "react"
-import DatePicker from "react-datepicker"
+import DatePicker, { registerLocale } from "react-datepicker"
+import ru from "date-fns/locale/ru" // the locale you want
 import { useAppDispatch } from "../../app/hooks"
 import { fetchHotelsRoutine } from "../../features/hotel/hotelSlice"
 import { addDays } from "../../util/util"
@@ -7,14 +8,23 @@ import calendarIcon from "../../asset/search/calendar.svg"
 import styles from "./Search.module.scss"
 import "react-datepicker/dist/react-datepicker.css"
 
+registerLocale("ru", ru)
+
 export function Search() {
   const [date, setDate] = useState(new Date())
   const [loc, setLoc] = useState("Москва")
   const [days, setDays] = useState(1)
+  const [error, setError] = useState("")
   const dispatch = useAppDispatch()
 
   const onSubmit = async (e: FormEvent) => {
     e.preventDefault()
+    setError("")
+    if (days <= 0 || days > 100) {
+      setError("Установите корректное количестово дней")
+      return
+    }
+
     const newDate = addDays(date, days)
     dispatch(
       fetchHotelsRoutine({
@@ -58,20 +68,27 @@ export function Search() {
       <form onSubmit={onSubmit} id="external-form">
         <div className={styles.textField}>
           <label htmlFor="location">Локация</label>
-          <input type="text" value={loc} onChange={(e) => setLoc(e.target.value)} />
+          <input type="text" value={loc} onChange={(e) => setLoc(e.target.value)} required />
         </div>
         <div className={styles.textField}>
           <label htmlFor="datepicker">Дата заселения</label>
           <DatePicker
             id="datepicker"
+            locale="ru"
             selected={date}
+            minDate={new Date()}
+            maxDate={addDays(new Date(), 90)}
             onChange={(date: Date) => setDate(date)}
             customInput={React.createElement(CustomInput)}
           />
         </div>
         <div className={styles.textField}>
           <label htmlFor="days">Количество дней</label>
-          <input id="days" type="number" value={days} onChange={(e) => setDays(Number(e.target.value))} />
+          <input id="days"
+                 type="number"
+                 value={days}
+                 onChange={(e) => setDays(Number(e.target.value))} />
+          {error && <span role="alert">{error}</span>}
         </div>
         <button type="submit" className={styles.searchButton}>
           Найти
